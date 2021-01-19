398ca342b6ff64144195c84fdd20ab2ede24ffa2
Dhanraj-Naik <naik.dhanraj01@gmail.com>
2021-01-19 22:07:19 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <Text style={styles.highlight}>App.js</Text>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
