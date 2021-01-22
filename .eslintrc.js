4a34d4b794f2bfa173b50da700a2e2c7a17e9e19
Dhanraj-Naik <naik.dhanraj01@gmail.com>
2021-01-22 14:05:11 +0530

    -lint to disable prettier -lint to use single quotes only

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    'prettier/prettier': 0, // prettier is disabled
+    'jsx-quotes': [2, 'prefer-single'],
+  },
 };
